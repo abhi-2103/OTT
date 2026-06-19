e85bc3fd40e0bcd5f78c6448fc2241ebdcab440f
abhi-2103 <kulkaraniabhilsh@gmail.com>
2026-06-19 10:09:49 +0530

    Added movies.js with 16 Telugu movies dataset

---
+++ src/data/movies.js
@@ -0,0 +1,98 @@
+export const moviesLineup = [
+  {
+    id: 1,
+    title: "Ala Vaikuntapuramuloo",
+    genre: "Comedy | Drama",
+    img: "https://i.pinimg.com/736x/61/fa/97/61fa97e3c2d7ce72d6182499d1809791.jpg"
+  },
+  {
+    id: 2,
+    title: "Aravindha Sametha",
+    genre: "Action | Drama",
+    img: "https://i.pinimg.com/736x/01/75/cd/0175cdf819eaf8f780eb4a4f9305d23c.jpg"
+  },
+  {
+    id: 3,
+    title: "Jalsa",
+    genre: "Action | Romance",
+    img: "https://i.pinimg.com/736x/44/6f/52/446f522fd7e6f8b27d0ace87c2853d10.jpg"
+  },
+  {
+    id: 4,
+    title: "Athadu",
+    genre: "Action | Comedy",
+    img: "https://i.pinimg.com/736x/d2/fb/0e/d2fb0e6fbd6d312312bf5cf682232cdc.jpg"
+  },
+  {
+    id: 5,
+    title: "Rangasthalam",
+    genre: "Action | Drama",
+    img: "https://i.pinimg.com/1200x/b8/b1/84/b8b18452a4e186b3b5be5d8be2000687.jpg"
+  },
+  {
+    id: 6,
+    title: "Mirchi",
+    genre: "Action | Drama",
+    img: "https://i.pinimg.com/1200x/91/01/50/910150d2dd57c7c1ba947675d75ed11c.jpg"
+  },
+  {
+    id: 7,
+    title: "Jersey",
+    genre: "Drama | Sports",
+    img: "https://i.pinimg.com/736x/bb/f6/98/bbf698e646eefb736099caa33c049192.jpg"
+  },
+  {
+    id: 8,
+    title: "Vakeel Saab",
+    genre: "Court Drama | Action",
+    img: "https://i.pinimg.com/1200x/06/9c/fb/069cfb0f7d97d757f78dfdf325f72c20.jpg"
+  },
+  {
+    id: 9,
+    title: "Krack",
+    genre: "Action | Comedy",
+    img: "https://i.pinimg.com/736x/84/d4/1b/84d41b2dcfffacc27e421dda4f687ee3.jpg"
+  },
+  {
+    id: 10,
+    title: "Temper",
+    genre: "Action",
+    img: "https://i.pinimg.com/736x/16/b9/4a/16b94a2ffa19889632a35318aed96c06.jpg"
+  },
+  {
+    id: 11,
+    title: "Legend",
+    genre: "Action | Drama",
+    img: "https://i.pinimg.com/736x/02/70/31/027031c0c62448452d69fa6df5923fc9.jpg"
+  },
+  {
+    id: 12,
+    title: "Hi Nanna",
+    genre: "Romance | Drama",
+    img: "https://i.pinimg.com/736x/b6/71/48/b6714847bbb3e8fecbea6fc7e4e2f085.jpg"
+  },
+  {
+    id: 13,
+    title: "They Call Him OG",
+    genre: "Action | Thriller",
+    img: "https://i.pinimg.com/1200x/69/83/95/698395d8145ed23aa842d00828f48203.jpg"
+  },
+  {
+    id: 14,
+    title: "Little Hearts",
+    genre: "Romance | Comedy",
+    img: "https://i.pinimg.com/1200x/60/61/6f/60616f367ea7d11458ea9e3474fa0008.jpg"
+  },
+  {
+    id: 15,
+    title: "Court",
+    genre: "Court Drama | Romance",
+    img: "https://i.pinimg.com/736x/b6/70/80/b670809a5e4833643494f212026822fb.jpg"
+  },
+  {
+    id: 16,
+    title: "Daaku Maharaaj",
+    genre: "Action | Drama",
+    img: "https://i.pinimg.com/736x/70/c1/fb/70c1fbebc2b7416302c96b23385eb0ac.jpg"
+  }
+];
